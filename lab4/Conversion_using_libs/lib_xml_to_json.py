f4db66e4024b76b2e7cb580546a401dce25d33c0
Boris Dvorkin <b_dvorkin@mail.ru>
2022-11-01 16:31:41 +0300

    Delete lab4/Conversion_using_libs directory

--- lab4/Conversion_using_libs/lib_xml_to_json.py
+++
@@ -1,7 +0,0 @@
-import json
-import xmltodict
-with open("Schedule.xml", encoding="utf-8") as xml_file:
-    data_dict = xmltodict.parse(xml_file.read())
-    json_data = json.dumps(data_dict)
-    with open("data.json", "w") as json_file:
-        json_file.write(json_data)
